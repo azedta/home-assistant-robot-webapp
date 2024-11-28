10ad16a3ff21acd417c4854f27173e8835d627c6
Aziz Tazrout <aztazrout@gmail.com>
2024-11-28 08:26:14 -0500

    Create the spring boot project for our web app

---
+++ home-assistant-robot-webapp/src/main/java/com/azedta/homebuddy/HomeAssistantRobotWebappApiApplication.java
@@ -0,0 +1,13 @@
+package com.azedta.homebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAssistantRobotWebappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAssistantRobotWebappApiApplication.class, args);
+	}
+
+}
