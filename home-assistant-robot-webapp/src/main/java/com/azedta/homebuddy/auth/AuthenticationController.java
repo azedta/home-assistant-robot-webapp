48ba7942a099cffcd9dd3cd77fb2d008d5f570a5
Aziz Tazrout <aztazrout@gmail.com>
2024-11-28 18:01:22 -0500

    Create the authentication controller

---
+++ home-assistant-robot-webapp/src/main/java/com/azedta/homebuddy/auth/AuthenticationController.java
@@ -0,0 +1,26 @@
+package com.azedta.homebuddy.auth;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Tag(name="Authentication")
+public class AuthenticationController {
+
+    private final AuthenticationServie service;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request
+    ) {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
+}
---
+++ home-assistant-robot-webapp/src/main/java/com/azedta/homebuddy/auth/AuthenticationServie.java
@@ -0,0 +1,10 @@
+package com.azedta.homebuddy.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServie {
+
+    public void register(RegistrationRequest request) {
+    }
+}
